0f19b431ce1e002433d2b50c757a268019fc4999
πwi <34057896+Tetramethylmethan@users.noreply.github.com>
2020-06-22 18:40:40 +0200

    Testweise H2DB hinzugefügt. DB ist noch leer

---
+++ WirMachenEinJavaProjekt/src/main/java/test/MainDBConnectTest.java
@@ -0,0 +1,53 @@
+package test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainDBConnectTest {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:./wmejpTest";
+
+    //  Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 3: Execute a query
+            stmt = conn.createStatement();
+            String sql =  "Create Table ups";
+            stmt.executeUpdate(sql);
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch(SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch(Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try{
+                if(stmt!=null) stmt.close();
+            } catch(SQLException se2) {
+            } // nothing we can do
+            try {
+                if(conn!=null) conn.close();
+            } catch(SQLException se){
+                se.printStackTrace();
+            } //end finally try
+        } //end try
+    }
+}
