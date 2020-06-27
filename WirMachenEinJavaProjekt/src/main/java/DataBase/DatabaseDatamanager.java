74102dffee3eec0520f67845a0d9e7f15d954a16
Insa Belter <insa.belter@gmx.de>
2020-06-27 09:46:56 +0200

    changed insert and delete

--- WirMachenEinJavaProjekt/src/main/java/DataBase/DatabaseDatamanager.java
+++ WirMachenEinJavaProjekt/src/main/java/DataBase/DatabaseDatamanager.java
@@ -48,13 +48,9 @@ public class DatabaseDatamanager {
             // STEP 4: Clean-up environment
             conn.close();
 
-        } catch(SQLException se) {
-
+        } catch(SQLException | ClassNotFoundException se) {
             se.printStackTrace();
-        } catch(Exception e) {
-            e.printStackTrace();
         } finally {
-
             try {
                 if(conn!=null) conn.close();
             } catch(SQLException se){
@@ -63,97 +59,47 @@ public class DatabaseDatamanager {
         }
     }
 
-    static public void printArray(List<?> list){
-        for (Object x: list
-             ) {
-            System.out.println(x.toString());
-        }
-        System.out.println("");
-    }
+//    public void update(String TABLE,String SET, String WHERE){ //bsp:"UPDATE Fakultaet SET name='geaendert' WHERE Fakultaet_ID>20;"
+//        execStatement("UPDATE "+ TABLE + " SET "+ SET +" WHERE "+ WHERE+";");
+//    }
 
-    public void update(String TABLE,String SET, String WHERE){ //bsp:"UPDATE Fakultaet SET name='geaendert' WHERE Fakultaet_ID>20;"
-        execStatement("UPDATE "+ TABLE + " SET "+ SET +" WHERE "+ WHERE+";");
-    }
+//    //legacy
+//    public void insert(String TABLE,String COLUMNS, String VALUES){ // legacy! bsp:"INSERT INTO Fakultaet(name)VALUES('technik');"
+//        execStatement("INSERT INTO "+ TABLE + "("+ COLUMNS +")VALUES("+ VALUES+");");
+//    }
 
-    //legacy
-    public void insert(String TABLE,String COLUMNS, String VALUES){ // legacy! bsp:"INSERT INTO Fakultaet(name)VALUES('technik');"
-        execStatement("INSERT INTO "+ TABLE + "("+ COLUMNS +")VALUES("+ VALUES+");");
-    }
-
-    public void insert(Object hasIDObjekt,DataManager dm){ //inseriert das übergebene Objekt in DM und DB. Duplizierter code kann durch legacy-methode umgangen werden
-        String Table = null;
-
-        if(hasIDObjekt instanceof HasID){
-
-            if(hasIDObjekt instanceof Student){
-                Student castobj = ((Student) hasIDObjekt);
+    public void insert(HasID object, DataManager dm){ //inseriert das übergebene Objekt in DM und DB. Duplizierter code kann durch legacy-methode umgangen werden
+            if(object instanceof Student){
+                Student castobj = ((Student) object);
                 dm.lsStudent.list.add(castobj);
+                castobj.getKurs().addSlave(castobj);
                 execStatement("INSERT INTO STUDENT(VORNAME,NACHNAME,JAVAKENNTNISSE,KURS_ID,FIRMA)VALUES('"+castobj.getVorname()+"','"+castobj.getNachname()+"',"+castobj.getJavakenntnisse()+","+castobj.getKurs().getId()+",'"+castobj.getFirma()+"');");
-
-            }else if(hasIDObjekt instanceof Kurs){
-                Kurs castobj = ((Kurs) hasIDObjekt);
+            }else if(object instanceof Kurs){
+                Kurs castobj = ((Kurs) object);
                 dm.lsKurs.list.add(castobj);
+                castobj.getStudienrichtung().addSlave(castobj);
                 execStatement("INSERT INTO KURS(JAHRGANG,NUMMER,RAUM,EMAILVERTEILER,STUDIENRICHTUNG_ID)VALUES("+castobj.getJahrgang()+","+castobj.getNummer()+",'"+castobj.getRaum()+"','"+castobj.getEmailVerteiler()+"',"+castobj.getStudienrichtung().getId()+");");
-
-            }else if(hasIDObjekt instanceof Studienrichtung){
-                Studienrichtung castobj = ((Studienrichtung) hasIDObjekt);
-                dm.lsStudienrichtung.list.add(castobj);
-                execStatement("INSERT INTO STUDIENRICHTUNG(NAME,KUERZEL,STUDIENGANG_ID)VALUES('"+castobj.getName()+"','"+castobj.getKuerzel()+"',"+castobj.getStudiengang().getId()+");");
-
-            }else if(hasIDObjekt instanceof Studiengang){
-                Studiengang castobj = ((Studiengang) hasIDObjekt);
-                dm.lsStudiengang.list.add(castobj);
-                execStatement("INSERT INTO STUDIENGANG(NAME,KUERZEL,STUDIENGANGSLEITER,FAKULTAET_ID)VALUES('"+castobj.getName()+"','"+castobj.getKuerzel()+"','"+castobj.getStudienGangsleiter()+"',"+castobj.getFakultaet().getId()+");");
-
-            }else if(hasIDObjekt instanceof Fakultaet){
-                Fakultaet castobj = ((Fakultaet) hasIDObjekt);
-                dm.lsFakultaet.list.add(castobj);
-                //execinsert("FAKULTAET","NAME","'"+ castobj.getName()+"'");
-                execStatement("INSERT INTO FAKULTAET(NAME)VALUES('"+ castobj.getName()+"');");
-            }
-        }else{
-            System.out.println("obj hat keine id");
         }
     }
     
-    public void delete(Object hasIDObjekt,DataManager dm){ //Übernimmt Objekt mit interface "hasID" un löscht es aus übergebnene dm und DB
+    public void delete(HasID object,DataManager dm){ //Übernimmt Objekt mit interface "hasID" un löscht es aus übergebnene dm und DB
+            int id = object.getId();
 
-        String Table = null;
-
-        if(hasIDObjekt instanceof HasID){
-
-            int id = ((HasID) hasIDObjekt).getId();
-
-            if(hasIDObjekt instanceof Student){
-                Table = "STUDENT";
-                dm.lsStudent.list.remove(hasIDObjekt);
-                execStatement("DELETE FROM "+ Table +" WHERE MATRIKEL_NR="+id+";");
-            }else{
-                if(hasIDObjekt instanceof Kurs){
-                    Table = "KURS";
-                    dm.lsKurs.list.remove(hasIDObjekt);
-
-                }else if(hasIDObjekt instanceof Studienrichtung){
-                    Table = "STUDIENRICHTUNG";
-                    dm.lsStudienrichtung.list.remove(hasIDObjekt);
-
-                }else if(hasIDObjekt instanceof Studiengang){
-                    Table = "STUDIENGANG";
-                    dm.lsStudiengang.list.remove(hasIDObjekt);
-
-                }else if(hasIDObjekt instanceof Fakultaet){
-                    Table = "FAKULTAET";
-                    dm.lsFakultaet.list.remove(hasIDObjekt);
-                }
-                execStatement("DELETE FROM "+ Table +" WHERE "+Table+"_ID ="+id+";");
+            if(object instanceof Student){
+                Student castobj = ((Student) object);
+                dm.lsStudent.list.remove(castobj);
+                castobj.getKurs().getStudents().remove(castobj);
+                execStatement("DELETE FROM STUDENT WHERE MATRIKEL_NR="+id+";");
+            }else if(object instanceof Kurs){
+                Kurs castobj = ((Kurs) object);
+                dm.lsKurs.list.remove(castobj);
+                castobj.getStudienrichtung().getKurse().remove(castobj);
+                execStatement("DELETE FROM KURS WHERE KURS_ID ="+id+";");
             }
-
-        }
-
     }
 
     //execStatement fürt ein beliebiges Statement ohne rückgabewert aus.
-    private void execStatement(String Statement){
+    public void execStatement(String Statement){
 
         //System.out.println(Statement);
         try {
